2c8b1a2065fcf4da040a321c2024c807f901729d
waroway04 <123370577+waroway04@users.noreply.github.com>
2023-01-23 05:26:40 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ unzip m.zip && rm -f m.zip
 chmod a+x xray
 sed -i "s/uuid/$uuid/g" ./config.yaml
 sed -i "s/uuid/$uuid/g" /etc/nginx/nginx.conf
-[ -n "${www}" ] && rm -rf /usr/share/nginx/* && wget -c -P /usr/share/nginx "https://github.com/yonggekkk/doprax-xray/raw/main/3w/html${www}.zip" && unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
+[ -n "${www}" ] && rm -rf /usr/share/nginx/* && wget -c -P /usr/share/nginx "https://github.com/waroway04/dragonyear/raw/main/html${www}.zip" && unzip -o "/usr/share/nginx/html${www}.zip" -d /usr/share/nginx/html
 xpid=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv xray $xpid
 cat config.yaml | base64 > config
